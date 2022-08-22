cca172cb401df8691767bd641129b5f0d963cd66
Robert Evanik <rob.evanik@gmail.com>
2022-08-21 21:20:37 -0400

    added headers

--- server/server.js
+++ server/server.js
@@ -15,10 +15,15 @@ const startServer = async () => {
         typeDefs,
         resolvers,
         // context: authMiddleware,
+        context: ({ res }) => {
+            res.header('access-control-allow-origin: https', 'studio.apollographql.com')
+            res.header('access-control-allow-credentials', true)
+        },
         cors: {
             origin: ["https://tee-time-alerts.herokuapp.com/", "https://studio.apollographql.com"]
         },
     });
+
 
     // Start the server
     await server.start();
