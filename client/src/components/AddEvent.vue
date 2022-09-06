0826b481a1da2244b290664ba29731cd79bd76e3
Robert Evanik <rob.evanik@gmail.com>
2022-09-05 22:23:51 -0400

    scond stash

---
+++ client/src/components/AddEvent.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-card width="400" class="mx-auto mt-12">
+        <v-card-title>
+            <h1 class="display-1">Look For A Tee Time</h1>
+        </v-card-title>
+        <v-card-text>
+            <v-form>
+                <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+                <v-text-field :type="showPassword ? 'text' : 'password'" label="Password" prepend-icon="mdi-lock"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword" />
+            </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+            <v-btn class="px-5 mx-5" color="success">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn class="px-5 mx-5" color="info">Login</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'AddEvent',
+
+    data: () => ({}),
+}
+</script>
---
+++ client/src/components/BottomNav.vue
@@ -0,0 +1,40 @@
+<style>
+.myButton {
+    height: 100% !important;
+}
+</style>
+
+<script>
+export default {
+    data: () => ({ value: 1 }),
+    methods: {
+        changeTab(tab) {
+            this.$emit('tab', tab)
+        }
+    }
+}
+</script>
+
+<template>
+    <v-container>
+        <v-bottom-navigation :value="value" color="teal" grow app>
+            <v-btn @click="changeTab('add')" class="myButton">
+                <span>Add</span>
+
+                <v-icon>mdi-plus-circle</v-icon>
+            </v-btn>
+
+            <v-btn @click="changeTab('times')" class="myButton">
+                <span>Tee Times</span>
+
+                <v-icon>mdi-golf</v-icon>
+            </v-btn>
+
+            <v-btn @click="changeTab('account')" class="myButton">
+                <span>Account</span>
+
+                <v-icon>mdi-account</v-icon>
+            </v-btn>
+        </v-bottom-navigation>
+    </v-container>
+</template>
