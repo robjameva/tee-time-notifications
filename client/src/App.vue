096c0899efc0fdf52bacf511f6164b688ff1d65c
Robert Evanik <rob.evanik@gmail.com>
2022-09-05 22:23:23 -0400

    fixed merge issues

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,31 @@
+<script>
+import BottomNav from './components/BottomNav';
+import AccountPage from './components/AccountPage';
+
+export default {
+  name: 'App',
+
+  components: {
+    BottomNav,
+    AccountPage
+  },
+
+  data: () => ({
+    currentTab: ''
+  }),
+  methods: {
+    setTab(tab) {
+      this.currentTab = tab
+    }
+  }
+};
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <v-app>
+    <v-main>
+      <Account-Page v-if="currentTab === 'account'" />
+    </v-main>
+    <Bottom-Nav @tab="setTab" />
+  </v-app>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
-createApp(App).use(router).mount("#app");
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
