adc9afe5a74518e8f6a3eac8a9e81ae098270b03
markcreasy <mcreasy50@gmail.com>
2022-09-11 21:22:06 -0400

    top bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import './App.css';
-import BottmNav from './components/BottomNav'
+import BottomNav from './components/BottomNav'
+import HeaderBar from './components/HeaderBar'
 import AddTeeTime from './pages/AddTeeTime';
 import TeeTimes from './pages/TeeTimes';
 import Account from './pages/Account';
@@ -10,28 +11,30 @@ function App() {
   return (
     <div className="App">
       <Router>
+        {/* TODO: Mark username dynamic */}
+        <HeaderBar username={'Mark'} />
         <div>
-            <Routes>
-              <Route
-                path="/"
-                element={<Home />}
-              />
-              <Route
-                path="/create-tee-time"
-                element={<AddTeeTime />}
-              />
-              <Route
-                path="/tee-times"
-                element={<TeeTimes />}
-              />
-              <Route
-                path="/account"
-                element={<Account />}
-              />
-            </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/create-tee-time"
+              element={<AddTeeTime />}
+            />
+            <Route
+              path="/tee-times"
+              element={<TeeTimes />}
+            />
+            <Route
+              path="/account"
+              element={<Account />}
+            />
+          </Routes>
         </div>
         <header className="App-header">
-          <BottmNav />
+          <BottomNav />
         </header>
       </Router>
     </div>
