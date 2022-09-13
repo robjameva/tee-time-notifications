b36024faddb685df96ce18d0f99d386c7d091703
Robert Evanik <rob.evanik@gmail.com>
2022-09-12 20:06:55 -0400

    started sign in/up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-// import './App.css';
 import BottmNav from './components/BottomNav'
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import ResponsiveAppBar from './components/AppBar';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <BottmNav />
-      </header>
-    </div>
+    <Router>
+      <ResponsiveAppBar />
+      <div className="App">
+        <Switch>
+          {/* <Route exact path="/" component={SignIn} /> */}
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+
+
+        </Switch>
+      </div>
+      <BottmNav />
+    </Router>
   );
 }
 
