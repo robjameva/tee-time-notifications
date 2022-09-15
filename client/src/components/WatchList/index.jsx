6daa3321fd5e5d28ec0def2fa5037e173458c393
Robert Evanik <rob.evanik@gmail.com>
2022-09-14 21:59:27 -0400

    styled the tee times watch list page

--- client/src/components/WatchList/index.jsx
+++ client/src/components/WatchList/index.jsx
@@ -9,47 +9,46 @@ import Typography from '@mui/material/Typography';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
 import CopyAllIcon from '@mui/icons-material/CopyAll';
-import sunset from '../../assets/images/sunset.png'
 
-export default function MediaControlCard({ course, start_time, end_time, num_golfers }) {
+export default function MediaControlCard({ course, courseLogo, date, start_time, end_time, num_golfers }) {
 
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 1, marginBottom: 1 }} >
-            <Card sx={{ display: 'flex' }} variant="outlined">
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography component="div" variant="h4">
-                            {course}
-                        </Typography>
-                        <Typography component="div" variant="h5">
-                            {start_time}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {start_time}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {end_time}
-                        </Typography>
-                    </CardContent>
-                    <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-                        <IconButton aria-label="edit">
-                            <EditIcon color="primary" />
-                        </IconButton>
-                        <IconButton aria-label="duplicate">
-                            <CopyAllIcon color="warning" />
-                        </IconButton>
-                        <IconButton aria-label="delete">
-                            <DeleteForeverIcon color="error" />
-                        </IconButton>
-                    </Box>
+        <Card sx={{ display: 'flex', width: '95%', 'justify-content': 'space-between', 'box-shadow': '1px 1px 10px -1px' }} variant="outlined">
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                    <Typography component="div" variant="h5">
+                        {course}
+                    </Typography>
+                    <Typography component="div" variant="h6">
+                        {date}
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                        {start_time} - {end_time}
+                    </Typography>
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                        {num_golfers} golfers
+                    </Typography>
+                </CardContent>
+                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                    <IconButton aria-label="edit">
+                        <EditIcon color="primary" />
+                    </IconButton>
+                    <IconButton aria-label="duplicate">
+                        <CopyAllIcon color="warning" />
+                    </IconButton>
+                    <IconButton aria-label="delete">
+                        <DeleteForeverIcon color="error" />
+                    </IconButton>
                 </Box>
+            </Box>
+            <div class='course-logo'>
                 <CardMedia
                     component="img"
-                    sx={{ width: 151, backgroundColor: 'success.light', 'object-fit': 'contain' }}
-                    image={sunset}
+                    sx={{ width: 175, height: 175, backgroundColor: 'success.light', 'object-fit': 'contain', 'border-radius': '3%' }}
+                    image={courseLogo}
                     alt="Live from space album cover"
                 />
-            </Card>
-        </Box>
+            </div>
+        </Card>
     );
 }
--- client/src/pages/TeeTimes.js
+++ client/src/pages/TeeTimes.js
@@ -1,35 +1,45 @@
 import React, { useState } from "react";
+import Box from '@mui/material/Box';
 import WatchList from '../components/WatchList'
+import sunsetLogo from '../assets/images/sunset.png'
+import birkshireLogo from '../assets/images/birkshire.png'
+import pinchBrookLogo from '../assets/images/pinchBrook.png'
+import flandersLogo from '../assets/images/flanders.png'
 
 const Home = () => {
   const [teeTimes, setTeeTimes] = useState([
     {
       course: 5152,
       start_time: '6:00am',
+      date: 'Sun, Sept 18th',
       end_time: '8:30am',
       num_golfers: 4
     },
     {
       course: 5153,
       start_time: '6:00am',
+      date: 'Sun, Sept 18th',
       end_time: '8:30am',
       num_golfers: 4
     },
     {
       course: 5150,
       start_time: '6:00am',
+      date: 'Sun, Sept 18th',
       end_time: '8:30am',
       num_golfers: 4
     },
     {
       course: 5151,
       start_time: '6:00am',
+      date: 'Sun, Sept 18th',
       end_time: '8:30am',
       num_golfers: 4
     },
     {
       course: 9535,
       start_time: '6:00am',
+      date: 'Sun, Sept 18th',
       end_time: '8:30am',
       num_golfers: 4
     },
@@ -52,12 +62,31 @@ const Home = () => {
     }
   }
 
+  function getCourseLogo(courseID) {
+    switch (courseID) {
+      case 5151:
+        return flandersLogo;
+      case 9535:
+        return flandersLogo;
+      case 5150:
+        return birkshireLogo;
+      case 5153:
+        return sunsetLogo;
+      case 5152:
+        return pinchBrookLogo;
+      default:
+        return 'Undefined Course';
+    }
+  }
+
   return (
     <div className="container">
       <h1>Tee Times</h1>
       {teeTimes.length ?
         teeTimes.map((teeTime, index) => (
-          <WatchList course={getCourseName(teeTime.course)} start_time={teeTime.start_time} end_time={teeTime.end_time} num_golfers={teeTime.num_golfers} />
+          <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2, marginBottom: 1 }} >
+            <WatchList course={getCourseName(teeTime.course)} courseLogo={getCourseLogo(teeTime.course)} date={teeTime.date} start_time={teeTime.start_time} end_time={teeTime.end_time} num_golfers={teeTime.num_golfers} />
+          </Box>
         )) : <h1>No Tee Times</h1>}
 
     </div>
