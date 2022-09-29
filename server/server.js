3c4ae7afc2e39df3d4911691f5a5e8a3b38dbd28
Robert Evanik <rob.evanik@gmail.com>
2022-09-28 22:29:15 -0400

    Added introsepction to server

--- server/server.js
+++ server/server.js
@@ -15,15 +15,15 @@ const startServer = async () => {
         typeDefs,
         resolvers,
         // context: authMiddleware,
-        // context: ({ res }) => {
-        //     res.header('access-control-allow-origin', 'https://studio.apollographql.com')
-        //     res.header('access-control-allow-credentials', true)
-        //     res.header('access-control-allow-methods', 'POST')
-        // },
+        context: ({ res }) => {
+            res.header('access-control-allow-origin', 'https://studio.apollographql.com')
+            //     res.header('access-control-allow-credentials', true)
+            //     res.header('access-control-allow-methods', 'POST')
+        },
         // cors: {
         //     origin: ["https://tee-time-alerts.herokuapp.com/", "https://studio.apollographql.com"]
         // },
-        // introspection: true
+        introspection: true
     });
 
 
